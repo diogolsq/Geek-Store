ac52b9357f4e149ea3d0f9337d425410c9389e0e
diogolsq <diogolsq@poli.ufrj.br>
2021-03-31 14:11:26 -0300

    adding remove feature to the checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -18,11 +18,11 @@ export const selectCartItems = createSelector(
 
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
-    cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity +cartItem.quantity, 0)
+    cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
     );
 
 
 export const selectCartTotal = createSelector(
     [selectCartItems],
-    cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity*cartItem.price, 0)
+    cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity * cartItem.price, 0)
 )
