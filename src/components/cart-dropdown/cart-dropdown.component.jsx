1cd6435ec6b83f3fa31f0642e3028ba67ad37c46
diogolsq <diogolsq@poli.ufrj.br>
2021-03-31 11:44:46 -0300

    starting creating checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,14 +6,19 @@ import CartItem from '../cart-item/cart-item.component';
 import { connect } from 'react-redux';
 import {selectCartItems } from '../../redux/cart/cart.selectors';
 
-const CartDropdown = ({cartItems}) => (
+import { withRouter } from 'react-router-dom';
+
+const CartDropdown = ({cartItems, history}) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
-    {cartItems &&
-      cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+    {cartItems.size > 0 ?
+      (cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />))
+      : (
+        <span className='empty-message'> Your Cart is empty</span>
+      )
     }  
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>>
+    <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>>
   </div>
 
 )
@@ -26,6 +31,6 @@ const mapStateToProps = state => ({
   cartItems: selectCartItems(state)
 })
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
 
 
