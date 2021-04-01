01265494fdb3a8bc978389f24365a534814648a1
diogolsq <diogolsq@poli.ufrj.br>
2021-04-01 14:41:35 -0300

    starting to create category page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.jsx';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { connect } from 'react-redux';
-import {createStructuredSelector} from 'reselect';
-import {selectCollections} from '../../redux/shop/shop.selectors';
+// import SHOP_DATA from './shop.data.jsx';
+// import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+// import { connect } from 'react-redux';
+// import {createStructuredSelector} from 'reselect';
+// import {selectCollections} from '../../redux/shop/shop.selectors';
+
+// lets nest route
+
+import { Route } from 'react-router-dom';
+import CategoryPage from '../../pages/category/category.component'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 // will refactor using redux states and not react states
@@ -33,11 +38,13 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 //   }
 // }
 
-
-const ShopPage = ({collections}) => {
+// since is already nested we have access to match, location and history props
+const ShopPage = ({match}) => {
+  console.log(match);
   return(
     <div className='shop-page'>
-      <CollectionsOverview />
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:categoryId`} component={CategoryPage}/>
     </div>
   )
 }
