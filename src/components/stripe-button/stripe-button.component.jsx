496c4f2d97368619a570bd5640897c9fb1efdb66
diogolsq <diogolsq@poli.ufrj.br>
2021-04-02 17:01:32 -0300

    hotfix logo not in the right place

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-
+import logo from '../../assets/logo.svg';
 
 
 const StripeCheckoutButton = ({ price }) => {
@@ -9,12 +9,22 @@ const StripeCheckoutButton = ({ price }) => {
     const publishabeKey = 'pk_test_51Ibo1EBiQBupP5Iu6grIsPWagkRmzsLahebhznCd4vV2n22LI0OGzqLjeE9lZPalc0mGIc4JQUVZ1TfbOsFzW1vh00iUcPItmt';
     
     
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Succesful')
+    }
+
     return (
         <StripeCheckout
             label='Pay Now'
             name='Geek Store Ltd.'
             billingAddress
             shippingAddress
+            image={logo}
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay now'
+            token={onToken}
         />
     )
 }
