6b4d39a1cf1b34de95b204c57952bd6dcca25bb5
diogolsq <diogolsq@poli.ufrj.br>
2021-04-07 16:48:15 -0300

    hotfix firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,14 +4,16 @@ import 'firebase/auth';
 
 
 const config = {
-    apiKey: process.env.REACT_APP_Firebase_apiKey,
-    authDomain: process.env.REACT_APP_Firebase_authDomain,
-    projectId: process.env.REACT_APP_Firebase_projectId,
-    storageBucket: process.env.REACT_APP_Firebase_storageBucket,
-    messagingSenderId: process.env.REACT_APP_Firebase_messagingSenderId,
-    appId: process.env.REACT_APP_Firebase_appId,
-    measurementId: process.env.REACT_APP_Firebase_measurementId
+  apiKey: process.env.REACT_APP_Firebase_apiKey,
+  authDomain: process.env.REACT_APP_Firebase_authDomain,
+  projectId: process.env.REACT_APP_Firebase_projectId,
+  storageBucket: process.env.REACT_APP_Firebase_storageBucket,
+  messagingSenderId: process.env.REACT_APP_Firebase_messagingSenderId,
+  appId: process.env.REACT_APP_Firebase_appId,
+  measurementId: process.env.REACT_APP_Firebase_measurementId
 }
+
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if(!userAuth) return;
@@ -55,7 +57,6 @@ export const addCollectionAndDocuments = (collectionKey,  objectsToAdd) => {
 
 
 
-firebase.initializeApp(config);
 
 
 
