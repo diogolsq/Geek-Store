fdd0a6990c98c922aab2f4f2b1b68002894a854d
diogolsq <diogolsq@poli.ufrj.br>
2021-04-10 09:14:23 -0300

    beggining to fix the woman page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -26,6 +26,5 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = collectionUrlParam =>
     createSelector(
         [selectCollections],
-        collections => collections.find(collection => collection.routeName === collectionUrlParam
-        )
+        collections => (collections ? collections.find(collection => collection.routeName === collectionUrlParam) : null)
     );
