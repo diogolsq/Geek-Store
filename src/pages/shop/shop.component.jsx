4ac7b74896bbaddf037db5adf8cabf4242c60092
diogolsq <diogolsq@poli.ufrj.br>
2021-04-14 11:21:53 -0300

    writing another way to get the data from firestore

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -138,6 +138,13 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
+    // // exemple how we would do using fetch
+    // fetch("https://firestore.googleapis.com/v1/projects/e-commercer-c9213/databases/(default)/documents/collections")
+    // .then(response => response.json())
+    // .then(collections => console.log(collections))
+
+
+
     collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
