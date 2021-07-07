c1a0a2cebb9f4e71ef3919f48262e55e4f9f7d94
diogolsq <diogolsq@poli.ufrj.br>
2021-07-07 09:26:26 -0300

    making pass through saga google sign in, TODO fix the sign in through email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -11,65 +11,76 @@ import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
 import { googleSignInStart } from '../../redux/user/user.actions'
 
 
-const SignIn = () => {
+class SignIn extends React.Component {
 
-  const  [ userCredentials, setCredentials ] = useState({ email: '', password: ''})
-  
-  const { email, password } = userCredentials;
-  
-  const handleSubmit = async (event) => {
-    event.preventDefault();
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      setCredentials({
-        email:'',
-        password:''})
-    }catch(error){
-      console.log(error);
-    }
+  constructor(props) {
+    super(props);
 
-    setCredentials({email: '', password: ''})
-  };
-
-  const handleChange = event => {
-    const { value, name } = event.target;
-
-    setCredentials({...userCredentials, [name]: value})
+    this.state = {
+      email: '',
+      password: ''
+    };
   }
 
-  // const { googleSignInStart } = this.props;
-    return(
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
-
-        <form onSubmit={handleSubmit}>
-          <FormInput name='email'
-          type='email'
-          handleChange={handleChange}
-          value={email}
-          label='email'
-          required />
-
-          <FormInput name='password'
-          type='password'
-          value={password}
-          label='password'
-          handleChange={handleChange}
-          required />
-          <div className='buttons'>
-            <CustomButton type='submit'> Sign In </CustomButton>
-            {/* <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn> Sign In with Google </CustomButton> */}
-            <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn> Sign In with Google </CustomButton>
-
-          </div>
-        </form>
-
-      </div>
-
-      )
+  // const  [ userCredentials, setCredentials ] = useState({ email: '', password: ''})
   
+  // const { email, password } = userCredentials;
+  
+  handleSubmit = async (event) => {
+    // event.preventDefault();
+
+    // try {
+    //   await auth.signInWithEmailAndPassword(email, password);
+    //   setCredentials({
+    //     email:'',
+    //     password:''})
+    // }catch(error){
+    //   console.log(error);
+    // }
+
+    // setCredentials({email: '', password: ''})
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    const { googleSignInStart } = this.props;
+      return(
+        <div className='sign-in'>
+          <h2>I already have an account</h2>
+          <span>Sign in with your email and password</span>
+
+          <form onSubmit={this.handleSubmit}>
+            <FormInput name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required />
+
+            <FormInput name='password'
+            type='password'
+            value={this.state.password}
+            label='password'
+            handleChange={this.handleChange}
+            required />
+            <div className='buttons'>
+              <CustomButton type='submit'> Sign In </CustomButton>
+              {/* <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn> Sign In with Google </CustomButton> */}
+              <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn> Sign In with Google </CustomButton>
+
+            </div>
+          </form>
+
+        </div>
+
+        )
+  }
 
 }
 
